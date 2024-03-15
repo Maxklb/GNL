8ffcffa24a76ee7d22b48002e1d9c05a379143de
Maximilien Koch Le Breton <themaxklb@gmail.com>
2024-03-15 11:23:14 +0100

    ca marche pas mais jpush quand meme

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 12:51:37 by makoch-l          #+#    #+#             */
-/*   Updated: 2024/03/12 15:37:14 by makoch-l         ###   ########.fr       */
+/*   Updated: 2024/03/14 19:47:23 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+#endif
 
 # include <stdlib.h>
 # include <unistd.h>
@@ -29,6 +30,10 @@ typedef struct	s_list
 char	*get_next_line(int fd);
 t_list	*ft_lst_get_last(t_list *stash);
 int		found_newline(t_list *stash);
-int		ft_strlen(const char *str)
+int		ft_strlen(const char *str);
 void	generate_line(char **line, t_list *stash);
 void	free_stash(t_list *stash);
+void	clean_stash(t_list **stash);
+void	extract_line(t_list *stash, char **line);
+void	read_and_stash(int fd, t_list **stash);
+void	add_to_stash(t_list **stash, char *buffer, int readed);
--- main.c
+++ main.c
@@ -6,11 +6,13 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 16:09:46 by makoch-l          #+#    #+#             */
-/*   Updated: 2024/03/07 16:16:14 by makoch-l         ###   ########.fr       */
+/*   Updated: 2024/03/14 19:49:39 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
 
 int main()
 {
@@ -21,9 +23,9 @@ int main()
 	while (1)
 	{
 		line = get_next_line(fd);
-		if (line = NULL)
-			break;
 		printf("%s", line);
+		if (line == NULL)
+			break ;
 		free(line);
 	}
 	return (0);
