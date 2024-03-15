1a52fdeb0a6e653ede0927d36f27ebfff99497ff
Maximilien Koch Le Breton <themaxklb@gmail.com>
2024-03-15 11:24:43 +0100

    t

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,12 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 12:51:37 by makoch-l          #+#    #+#             */
-/*   Updated: 2024/03/14 19:47:23 by makoch-l         ###   ########.fr       */
+/*   Updated: 2024/03/15 11:24:18 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#endif
 
 # include <stdlib.h>
 # include <unistd.h>
@@ -37,3 +36,4 @@ void	clean_stash(t_list **stash);
 void	extract_line(t_list *stash, char **line);
 void	read_and_stash(int fd, t_list **stash);
 void	add_to_stash(t_list **stash, char *buffer, int readed);
+#endif
