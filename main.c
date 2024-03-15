deb5a82386fddeab287db27fe808615570f739e7
Maximilien Koch Le Breton <themaxklb@gmail.com>
2024-03-15 20:36:38 +0100

    added main

---
+++ main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 16:09:46 by makoch-l          #+#    #+#             */
+/*   Updated: 2024/03/14 19:49:39 by makoch-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int main()
+{
+	int		fd;
+	char	*line;
+
+	fd = open("tests/two_lines_with_nl", O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+		if (line == NULL)
+			break ;
+		free(line);
+	}
+	return (0);
+}
