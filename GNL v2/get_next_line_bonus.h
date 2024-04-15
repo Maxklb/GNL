aba0d835f5f217e23c0b280e2121f3f99a993955
Maximilien Koch Le Breton <themaxklb@gmail.com>
2024-04-15 17:25:58 +0200

    Added Bonuses

---
+++ GNL v2/get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 17:24:34 by makoch-l          #+#    #+#             */
+/*   Updated: 2024/04/15 17:25:41 by makoch-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5
+# endif
+
+int		ft_strlen(char *str);
+int		ft_strchr(char *stash, char c);
+char	*ft_strdup(char *src);
+char	*fill_line(char *tmp);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *stash, char *buffer);
+char	*read_fd(char *stash, int fd);
+char	*get_next_line(int fd);
+void	shift_stash(char *stash);
+
+#endif
---
+++ GNL v2/get_next_line_utils_bonus.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 17:25:23 by makoch-l          #+#    #+#             */
+/*   Updated: 2024/04/15 17:25:32 by makoch-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+	
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+	
+	i = 0;
+	while (src[i])
+		i++;
+	dest = malloc(sizeof(char) * (i + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+int	ft_strchr(char *stash, char c)
+{
+	int	i;
+
+	i = 0;
+	if (!stash)
+		return (0);
+	while (stash[i])
+	{
+		if (stash[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char *stash, char *buffer)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	str = malloc(sizeof(char) * (ft_strlen(stash) + ft_strlen(buffer) + 1));
+	if (!str)
+		return (NULL);
+	while (stash && stash[i])
+	{
+		str[i] = stash[i];
+		i++;
+	}
+	while (buffer[j])
+	{
+		str[i + j] = buffer[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	free(stash);
+	return (str);
+}
